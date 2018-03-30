8dc23f06ade7baaab4c1d218842453325c3b4184
Tigran Mirzoev <TigranMirzoev@gmail.com>
2018-03-30 15:28:12 +0300

    Minimal React playground.

---
+++ README.md
@@ -0,0 +1,12 @@
+# react-playground
+
+React development environment with webpack, babel, eslint, and mocha.
+
+## Features
+
+* React 16
+* Webpack 4
+* Babel
+* Staging ES Next Features
+* Hot Module Replacement
+
---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// cSpell:disable
+const title = 'Tig\'s React Playground';
+// cSpell:enable
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>{title}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
